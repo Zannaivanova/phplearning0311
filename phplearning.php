fccbcc544f020a85196f8ebbee7943103e279a3d
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-03 13:07:43 +0200

    Вывод значения переменной (элемента массива)

--- phplearning.php
+++ phplearning.php
@@ -3,12 +3,16 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Тестируем PHP</title>
+	<title>Вывод значения переменной (элемента массива)</title>
 </head>
 <body>
 	 
-<?php phpinfo(); ?>
 
-	 <!-- https://www.php.net/manual/ru/tutorial.firstpage.php
- --></body>
+<?php
+echo $_SERVER['HTTP_USER_AGENT'];
+?>
+
+
+<!-- 	https://www.php.net/manual/ru/tutorial.useful.php -->
+</body>
 </html>

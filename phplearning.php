8213f87fa829aabeb8559b6df5050bb5f5db6e04
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-03 12:24:22 +0200

    first page php

---
+++ phplearning.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Тестируем PHP</title>
+</head>
+<body>
+	 <?php echo '<p>Привет, мир!</p>'; ?>
+	 https://www.php.net/manual/ru/tutorial.firstpage.php
+</body>
+</html>

4f69133b2d5edc31828949643ca4fc6adcbf95cd
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-03 13:50:21 +0200

    Смешение режимов HTML и PHP

--- phplearning.php
+++ phplearning.php
@@ -3,15 +3,24 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Пример использования управляющих структур и функций</title>
+	<title>Смешение режимов HTML и PHP</title>
 </head>
 <body>
 	 
 
 
+
 <?php
-if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') == FALSE) {
-    echo 'Вы не используете Internet Explorer.<br />';
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
+?>
+<h3>strpos(), должно быть, вернул не false</h3>
+<p>Вы используете Internet Explorer</p>
+<?php
+} else {
+?>
+<h3>strpos() вернул false</h3>
+<p>Вы не используете Internet Explorer</p>
+<?php
 }
 ?>
 

0d8629a480384e708c8fa0380cb8d4a2dc61a52a
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-04 04:28:06 +0200

    Четыре скалярных типа:

--- phplearning.php
+++ phplearning.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Смешение режимов HTML и PHP</title>
+	<title>Типы PHP</title>
 </head>
 <body>
 	 
@@ -11,21 +11,31 @@
 
 
 <?php
-if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== FALSE) {
-?>
-<h3>strpos(), должно быть, вернул не false</h3>
-<p>Вы используете Internet Explorer</p>
-<?php
-} else {
-?>
-<h3>strpos() вернул false</h3>
-<p>Вы не используете Internet Explorer</p>
-<?php
+$a_bool = TRUE;
+$a_str = "foo";
+$a_str2 = 'foo';
+$an_int = 12;
+
+echo gettype($a_bool); 
+echo gettype($a_str);
+
+
+//Если целое число, увеличить на четыре
+if (is_int($an_int)){
+	$an_int +=4;
 }
+
+//Если $a_bool - это строка, вывести её
+if (is_string($a_bool)) {
+	echo "Строка: $a_bool";
+} 
+
+echo PHP_INT_MAX;
+
 ?>
 
 
 
-<!-- 	https://www.php.net/manual/ru/tutorial.useful.php -->
+<!-- 	https://www.php.net/manual/ru/language.types.intro.php-->
 </body>
 </html>

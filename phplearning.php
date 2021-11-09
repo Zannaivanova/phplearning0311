01e3e232c251e7d06b1e53cdcd830dabd8c6e43d
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-09 12:48:52 +0200

    4 смешанных типа: iterable

--- phplearning.php
+++ phplearning.php
@@ -3,27 +3,42 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>4 смешанных типа: array</title>
+	<title>4 смешанные типа: iterable</title>
 </head>
 <body>
 
+<!-- Пример #1 Пример использования iterable в качестве параметра -->
 
-<?php
-
-$colors = array('red', 'blue', 'green', 'yellow');
-
-
-foreach ($colors as &$color) {
-    $color = strtoupper($color);
+<?php 
+function foo(iterable $iterable){
+	foreach ($iterable as $value){
+		//...
+	}
 }
-unset($color); /* это нужно для того, чтобы последующие записи в
-$color не меняли последний элемент массива */
+ ?>
 
-print_r($colors);
-?>
+<!-- Пример #2 Пример установки значения по умолчанию для iterabl -->
+<?php 
+function foo (iterable $iterable = []){
+}	
+ ?>
+
+<!-- Пример #3 Пример использования iterable в качестве возвращаемого типа  -->
+<?php 
+function bar(): iterable{
+	return [1,2,3];
+} ?>
+
+<!-- Пример #4 Пример использования iterable в качестве возвращаемого значения генератора  -->
+<?php 
+function gen(): iterable{
+	yield 1;
+	yield 2;
+	yield 3;
+} ?>
 
 
+<!-- 	https://www.php.net/manual/ru/language.types.iterable.php->
 
-<!-- 	https://www.php.net/manual/ru/language.types.array.php-->
 </body>
 </html>

3ba97876cc531cbf1cf55fef3ca520a03bea0274
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-10 10:40:07 +0200

    2 специальных типа: resource

--- phplearning.php
+++ phplearning.php
@@ -3,25 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Перечисления: Enums</title>
+	<title>2 специальных типа: resource </title>
 </head>
 <body>
-<?php 
-enum Suit {
-	case Hearts;
-	case Diamonds;
-	case Clubs;
-	case Spades;
-}
 
-function do_stuff(Suit $s) {
+<?php
+// выводит: stream
+$fp = fopen("foo", "w");
+echo get_resource_type($fp) . "\n";
 
-}
-
-do_stuff(Suit::Spades);
- ?>
+// prints: curl
+$c = curl_init ();
+echo get_resource_type($c) . "\n"; // это работает до версии PHP 8.0.0 так как с версии 8.0 curl_init возвращает объект CurlHandle, а не ресурс
+?>
 
 
-<!-- https://www.php.net/manual/ru/language.types.enumerations.php -->
-</body>
+<!-- https://www.php.net/manual/ru/language.types.resource.php
+ -->
+ 	
+ </body>
 </html>

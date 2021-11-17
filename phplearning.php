5361c11fa190c56a52b5ae575328c23299f17617
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-17 06:48:04 +0200

    Константы: синтаксис

--- phplearning.php
+++ phplearning.php
@@ -3,51 +3,42 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Переменные извне PHP</title>
+	<title>Константы: синтаксис</title>
 </head>
 <body>
+<!-- Пример #1 Определение констант -->
+<?php  
+define("CONSTANT", "Здравствуй, мир");
+echo CONSTANT;
+echo Constant;
+?>
 
-    <!-- Пример #3 Более сложные переменные формы -->
+
+<!-- Пример #2 Определение констант с помощью ключевого слова const -->
+
 <?php
-if ($_POST) {
-    echo '<pre>';
-    echo htmlspecialchars(print_r($_POST, true));
-    echo '</pre>';
-}
-?>
-<form action="" method="post">
-    Имя:  <input type="text" name="personal[name]" /><br />
-    фото: <input type="image" src="image.gif" name="sub" />
-    Email: <input type="text" name="personal[email]" /><br />
-    Пиво: <br />
-    <select multiple name="beer[]">
-        <option value="warthog">Warthog</option>
-        <option value="guinness">Guinness</option>
-        <option value="stuttgarter">Stuttgarter Schwabenbräu</option>
-    </select><br />
-    <input type="submit" value="Отправь меня!" />
-</form>
+// Простое скалярное значение
+const CONSTANT = 'Здравствуй, мир.';
 
+echo CONSTANT;
 
-<!-- HTTP Cookies -->
-<?php  
-setcookie("MyCookie[foo]", 'Testing 1', time()+3600);
-setcookie("MyCookie[bar]", 'Testing 2', time()+3600);
-?>
+// Скалярное выражение
+const ANOTHER_CONST = CONSTANT.'; Прощай, мир.';
+echo ANOTHER_CONST;
 
-<!-- Пример #4 Пример использования setcookie() -->
-<?php  
-if (isset($_COOKIE['count'])){
-    $count = $_COOKIE['count']+1;
-} else {
-    $count = 1;
-}
-setcookie('count', $count, time()+3600);
-setcookie("Cart[$count]", $item, time()+3600);
+const ANIMALS = array('dog', 'cat', 'bird');
+echo ANIMALS[1]; // выводит "cat"
+
+// Массивы в константе
+define('ANIMALS', array(
+    'dog',
+    'cat',
+    'bird'
+));
+echo ANIMALS[1]; // выводит "cat"
 ?>
 
 
-
-<!-- https://www.php.net/manual/ru/language.variables.external.php -->
+<!-- https://www.php.net/manual/ru/language.constants.syntax.php -->
 </body>
 </html>

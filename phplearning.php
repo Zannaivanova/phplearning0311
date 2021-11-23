4a2a68d992a14b6fcc7e6e1f49c256a08801ddcf
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-23 14:57:05 +0200

    Константы классов

--- phplearning.php
+++ phplearning.php
@@ -3,92 +3,67 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Классы и объекты: Свойства</title>
+	<title>Константы классов</title>
 </head>
 <body>
 
-<?php  //Пример #1 Определение свойств
 
-
-class SimpleClass{
-	public $var = ''. '';
-	public $var2 = <<<EOD
-hello world
-EOD;
-    public $var3 = 1+2;
-
-       // неправильное определение свойств:
-    public $var4 = self::myStaticMethod();
-    public $var5 = $myVar;
-
-   // правильное определение свойств:
-    public $var6 = myConstant;
-    public $var7 = [true, false];
-
-    public $var8 = <<<'EOD'
-   hello world
-
-EOD;}
-?>
-
-
-<?php //Пример #2 Пример использования типизированных свойств
-class User{
-	public int $id;
-	public ?string $name;
-
-	public function __construct(int $id, ?string $name){
-		$this->id = $id;
-		$this->name = $name;
-	}
-}
- $user = new User(1234, null);
-
- var_dump($user->id);
- var_dump($user->name);
- ?>
-
-
-<?php//  Пример #3 Обращение к свойствам
-
-class Shape
+<?php  //Пример #1 Объявление и использование константы
+class MyClass
 {
-    public int $numberOfSides;
-    public string $name;
+    const CONSTANT = 'значение константы';
 
-    public function setNumberOfSides(int $numberOfSides): void
-    {
-        $this->numberOfSides = $numberOfSides;
-    }
-
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
-    public function getNumberOfSides(): int
-    {
-        return $this->numberOfSides;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
+    function showConstant() {
+        echo  self::CONSTANT . "\n";
     }
 }
 
-$triangle = new Shape();
-$triangle->setName("triangle");
-$triangle->setNumberofSides(3);
-var_dump($triangle->getName());
-var_dump($triangle->getNumberOfSides());
+echo MyClass::CONSTANT . "\n";
 
-$circle = new Shape();
-$circle->setName("circle");
-var_dump($circle->getName());
-var_dump($circle->getNumberOfSides());
+$classname = "MyClass";
+echo $classname::CONSTANT . "\n";
+
+$class = new MyClass();
+$class->showConstant();
+
+echo $class::CONSTANT."\n";
 ?>
 
-<!-- https://www.php.net/manual/ru/language.oop5.properties.php-->
+
+
+<?php  //Пример #2 Пример использования ::class с пространством имён
+namespace foo {
+    class bar {
+    }
+
+    echo bar::class; // foo\bar
+}
+?>
+
+
+<?php  //Пример #3 Пример констант, заданных выражением
+const ONE = 1;
+
+class foo{
+	const TWO = ONE *2;
+	const THREE = ONE + self::TWO;
+	const SENTENCE = 'значение константы THREE - '. self::THREE;
+}
+?>
+
+
+<?php  //Пример #4 Модификаторы видимости констант класса, начиная с PHP 7.1.0
+class Foo{
+	public const BAR = 'bar';
+	private const BAZ = 'baz';
+}
+
+echo Foo::BAR, PHP_EOL;
+echo Foo::BAZ, PHP_EOL;
+
+
+?>
+
+<!-- https://www.php.net/manual/ru/language.oop5.constants.php-->
 </body> 
 </html>

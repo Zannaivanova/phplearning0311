f2b24bceb72ffa7c36fd86977d9c5c2449538342
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-27 12:12:49 +0200

    Оператор разрешения области видимости (::)

--- phplearning.php
+++ phplearning.php
@@ -3,42 +3,60 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Наследование</title>
+	<title>Оператор разрешения области видимости (::)</title>
 </head>
 <body>
 
-<?php
 
-class Foo
-{
-    public function printItem($string)
-    {
-        echo 'Foo: ' . $string . PHP_EOL;
-    }
-
-    public function printPHP()
-    {
-        echo 'PHP просто супер.' . PHP_EOL;
-    }
+<?php//Пример #1 Использование :: вне объявления класса
+class MyClass{
+	const CONST_VALUE = 'Значение константы';
 }
 
-class Bar extends Foo
-{
-    public function printItem($string)
-    {
-        echo 'Bar: ' . $string . PHP_EOL;
-    }
-}
+$classname = 'MyClass';
+echo $classname::CONST_VALUE;
 
-$foo = new Foo();
-$bar = new Bar();
-$foo->printItem('baz'); // Выведет: 'Foo: baz'
-$foo->printPHP();       // Выведет: 'PHP просто супер'
-$bar->printItem('baz'); // Выведет: 'Bar: baz'
-$bar->printPHP();       // Выведет: 'PHP просто супер'
-
+echo MyClass::CONST_VALUE;
 ?>
 
-<!-- https://www.php.net/manual/ru/language.oop5.inheritance.php -->
+
+<?php  //Пример #2 Использование :: внутри объявления класса
+class OtherClass extends MyClass{
+	public static $my_static = 'статическая переменная';
+
+	public static function doubleColon(){
+		echo parent::CONST_VALUE . "\n";
+		echo self::$my_static . "\n";
+	}
+}
+
+$classname = 'OtherClass';
+$classname::doubleColon();
+
+OtherClass::doubleColon();
+?>
+
+
+
+<?php//Пример #3 Обращение к методу в родительском классе
+class MyClass{
+	protected function myFunc(){    // Переопределить родительское определение
+		echo "MyClass::myFunc()\n";
+	}
+}  
+
+class OtherClass extends MyClass{
+	public function myFunc(){        // Но всё ещё вызываем родительскую функцию
+		parent::myFunc();
+		echo "OtherClass::myFunc()\n";
+	}
+}
+
+$class = new OtherClass();
+$class->myFunc();
+?>
+
+
+<!-- https://www.php.net/manual/ru/language.oop5.paamayim-nekudotayim.php -->
 </body> 
 </html>

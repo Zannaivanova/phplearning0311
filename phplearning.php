d521d4edf79b92eb9cea4fbaf377f4c3d5ecfb92
Zannaivanova <Zannaivanova@tutanota.com>
2021-11-28 06:59:49 +0200

    Абстрактные классы

--- phplearning.php
+++ phplearning.php
@@ -3,54 +3,75 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Ключевое слово static</title>
+	<title>Абстрактные классы</title>
 </head>
 <body>
 
-<?php  //Пример #1 Пример статического метода
-class Foo{
-	public static function aStaticMethod(){
-		//...
-	}
+<?php //Пример #1 Пример абстрактного класса
+abstract class AbstractClass
+{
+   /* Данный метод должен быть определён в дочернем классе */
+    abstract protected function getValue();
+    abstract protected function prefixValue($prefix);
+
+   /* Общий метод */
+    public function printOut() {
+        print $this->getValue() . "\n";
+    }
 }
 
-Foo::aStaticMethod();
-$classname = 'Foo';
-$classname::aStaticMethod();
+class ConcreteClass1 extends AbstractClass
+{
+    protected function getValue() {
+        return "ConcreteClass1";
+    }
+
+    public function prefixValue($prefix) {
+        return "{$prefix}ConcreteClass1";
+    }
+}
+
+class ConcreteClass2 extends AbstractClass
+{
+    public function getValue() {
+        return "ConcreteClass2";
+    }
+
+    public function prefixValue($prefix) {
+        return "{$prefix}ConcreteClass2";
+    }
+}
+
+$class1 = new ConcreteClass1;
+$class1->printOut();
+echo $class1->prefixValue('FOO_') ."\n";
+
+$class2 = new ConcreteClass2;
+$class2->printOut();
+echo $class2->prefixValue('FOO_') ."\n";
 ?>
 
 
+<?php //Пример #2 Пример абстрактного класса
 
-<?php  //Пример #2 Пример статического свойства
-class Foo{
-	public static $my_static = 'foo';
-
-	public function staticValue(){
-		return self::$my_static;
-	}
+abstract class AbstractClass{    // Наш абстрактный метод требует только определить необходимые аргументы
+        abstract protected function prefixName($name);
 }
 
-class Bar extends Foo{
-	public function fooStatic(){
-		return parent::$my_static;
+class ConcreteClass extends AbstractClass{    // Наш дочерний класс может определить необязательные аргументы, не указанные в объявлении родительского метода
+	public function prefixName($name, $separator = "."){
+		if ($name == "Pacman"){
+			$prefix = "Mr";
+		} elseif ($name == "Pacwoman"){
+			$prefix = "Mrs"; 
+		} return "{$prefix}{$separator} {$name}";
 	}
-}
-
-print Foo::$my_static . "\n";
-
-$foo = new Foo();
-print $foo->staticValue() . "\n";
-print $foo->my_static . "\n";
-
-print $foo::$my_static . "\n";
-$classname = 'Foo';
-print $classname::$my_static . "\n";
-
-print Bar::$my_static . "\n";
-$bar = new Bar();
-print $bar->fooStatic() . "\n";
+} 
+$class = new ConcreteClass;
+echo $class->prefixName("Pacman"), "\n";
+echo $class-> prefixName("Pacwoman"), "\n";
 ?>
 
-<!-- https://www.php.net/manual/ru/language.oop5.static.php -->
+<!-- https://www.php.net/manual/ru/language.oop5.abstract.php -->
 </body> 
 </html>

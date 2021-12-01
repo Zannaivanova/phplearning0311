c32225f14fdb492def889a9bb7297c2b2935e906
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-01 16:53:44 +0200

    Итераторы объектов

--- phplearning.php
+++ phplearning.php
@@ -3,109 +3,41 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Перегрузка</title>
+	<title>Итераторы объектов</title>
 </head>
 <body>
- 
-<?php  //Пример #1 Перегрузка свойств с помощью методов __get(), __set(), __isset() и __unset() 
-class PropertyTest
+
+<?php //Пример #1 Итерация простого объекта
+class MyClass
 {
-    /**  Место хранения перегружаемых данных.  */
-    private $data = array();
+    public $var1 = 'значение 1';
+    public $var2 = 'значение 2';
+    public $var3 = 'значение 3';
 
-    /**  Перегрузка не применяется к объявленным свойствам.  */
-    public $declared = 1;
+    protected $protected = 'защищённая переменная';
+    private   $private   = 'закрытая переменная';
 
-    /**  Здесь перегрузка будет использована только при доступе вне класса.  */
-    private $hidden = 2;
-
-    public function __set($name, $value)
-    {
-        echo "Установка '$name' в '$value'\n";
-        $this->data[$name] = $value;
-    }
-
-    public function __get($name)
-    {
-        echo "Получение '$name'\n";
-        if (array_key_exists($name, $this->data)) {
-            return $this->data[$name];
-        }
-
-        $trace = debug_backtrace();
-        trigger_error(
-            'Неопределённое свойство в __get(): ' . $name .
-            ' в файле ' . $trace[0]['file'] .
-            ' на строке ' . $trace[0]['line'],
-            E_USER_NOTICE);
-        return null;
-    }
-
-    public function __isset($name)
-    {
-        echo "Установлено ли '$name'?\n";
-        return isset($this->data[$name]);
-    }
-
-    public function __unset($name)
-    {
-        echo "Уничтожение '$name'\n";
-        unset($this->data[$name]);
-    }
-
-    /**  Не магический метод, просто для примера. */
-    public function getHidden()
-    {
-        return $this->hidden;
+    function iterateVisible() {
+       echo "MyClass::iterateVisible:\n";
+       foreach ($this as $key => $value) {
+           print "$key => $value\n";
+       }
     }
 }
 
+$class = new MyClass();
 
-echo "<pre>\n";
-
-$obj = new PropertyTest;
-
-$obj->a = 1;
-echo $obj->a . "\n\n";
-
-var_dump(isset($obj->a));
-unset($obj->a);
-var_dump(isset($obj->a));
+foreach($class as $key => $value) {
+    print "$key => $value\n";
+}
 echo "\n";
 
-echo $obj->declared . "\n\n";
 
-echo "Давайте поэкспериментируем с закрытым свойством 'hidden':\n";
-echo "Закрытые свойства видны внутри класса, поэтому __get() не используется...\n";
-echo $obj->getHidden() . "\n";
-echo "Закрытые свойства не видны вне класса, поэтому __get() используется...\n";
-echo $obj->hidden . "\n";
-?>
+$class->iterateVisible();
+
+?> 
 
 
-<?php  // Пример #2 Перегрузка методов с помощью методов __call() и __callStatic() 
-class MethodTest {
-    public function __call($name, $arguments) {
-        // Замечание: значение $name регистрозависимо.
-        echo "Вызов метода '$name' "
-             . implode(', ', $arguments). "\n";
-    }
-
-    public static function __callStatic($name, $arguments) {
-        // Замечание: значение $name регистрозависимо.
-        echo "Вызов статического метода '$name' "
-             . implode(', ', $arguments). "\n";
-    }
-}
-
-$obj = new MethodTest;
-$obj->runTest('в контексте объекта');
-
-MethodTest::runTest('в статическом контексте');
-?>
-
-
-
-<!-- https://www.php.net/manual/ru/language.oop5.overloading.php -->
+<!-- https://www.php.net/manual/ru/language.oop5.iterations.php -->
 </body> 
 </html>

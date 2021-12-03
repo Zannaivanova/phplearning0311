9e6316adb6e480e0e603104a93caf571401ebf34
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-03 12:41:17 +0200

    Ковариантность и контравариантность

--- phplearning.php
+++ phplearning.php
@@ -3,33 +3,112 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Сериализация объектов</title>
+	<title>Ковариантность и контравариантность</title>
 </head>
 <body>
 
-<?php 
+<?php  //Ковариантность
+abstract class Animal
+{
+    protected string $name;
 
-class A {
-// classa.inc:
-    public $one = 1;
-
-    public function show_one(){
-        echo $this->one;
+    public function __construct(string $name)
+    {
+        $this->name = $name;
     }
-} 
 
-// page1.php:
+    abstract public function speak();
+}
 
-include("classa.inc");
+class Dog extends Animal
+{
+    public function speak()
+    {
+        echo $this->name . " лает";
+    }
+}
 
-$a = new A;
-$s = serialize($a);
+class Cat extends Animal
+{
+    public function speak()
+    {
+        echo $this->name . " мяукает";
+    }
+}
+?>
+<?php  
+interface AnimalShelter{
+    public function adopt (string $name): Animal;
+}
 
+class catShelter implements AnimalShelter{
+    public function adopt(string $name): Cat{// Возвращаем класс Cat вместо Animal
+        return new Cat($name);
+    }
+}
 
-  // сохраняем $s где-нибудь, откуда page2.php сможет его получить.
-file_put_contents('store', $s);
+class DogShelter implements AnimalShelter{
+    public function adopt(string $name): Dog{// Возвращаем класс Dog вместо Animal
+        return new Dog($name);
+    }
+}
+
+$kitty = (new CatShelter)->adopt("Рыжик");
+$kitty->speak();
+echo "\n";
+
+$doggy = (new DogShelter)->adopt("Бобик");
+$doggy->speak();
 ?>
 
-<!-- https://www.php.net/manual/ru/language.oop5.serialization.php -->
+
+
+
+<?php  //Контравариантность
+
+
+class Food {}
+
+class AnimalFood extends Food {}
+
+abstract class Animal
+{
+    protected string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function eat(AnimalFood $food)
+    {
+        echo $this->name . " ест " . get_class($food);
+    }
+}
+?>
+
+
+<?php
+
+class Dog extends Animal
+{
+    public function eat(Food $food) {
+        echo $this->name . " ест " . get_class($food);
+    }
+}
+?>
+
+<?php
+
+$kitty = (new CatShelter)->adopt("Рыжик");
+$catFood = new AnimalFood();
+$kitty->eat($catFood);
+echo "\n";
+
+$doggy = (new DogShelter)->adopt("Бобик");
+$banana = new Food();
+$doggy->eat($banana);?>
+
+<!-- https://www.php.net/manual/ru/language.oop5.variance.php -->
 </body> 
 </html>

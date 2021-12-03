bcc911fea01a98ebb800eeadb5ab0242b64e1cfd
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-03 09:48:02 +0200

    Объекты и ссылки

--- phplearning.php
+++ phplearning.php
@@ -3,112 +3,40 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Позднее статическое связывание</title>
+	<title>Объекты и ссылки</title>
 </head>
 <body>
 
-    <?php  //Пример #1 Использование self::
-    class A {
-        public static function who(){
-            echo __CLASS__;
-            }
-        public static function test(){
-            self::who();
-        }
-    }
-
-class B extends A{
-    public static function who(){
-        echo __CLASS__;
-    }
-}    
-
-B::test();
-?>
-
-
-<?php //Пример #2 Простое использование static::
+<?php  //Пример #1 Ссылки и объекты
 class A {
-    public static function who(){
-        echo __CLASS__;
-    }
-
-    public static function test(){
-        static::who();// Здесь действует позднее статическое связывание
-    }
-}  
-
-class B extends A{
-    public static function who(){
-        echo __CLASS__;
-    }
+    public $foo = 1;
 }
 
-B::test();
+$a=new A;
+$b = $a;// $a и $b - копии одного идентификатора
+             // ($a) = ($b) = <id>
+$b->foo = 2;
+echo $a->foo. "\n";
+
+$c = new A;
+$d = &$c; // $c и $d - ссылки
+             // ($c,$d) = <id>
+
+$d->foo = 2;
+echo $c->foo. "\n";
+
+$e = new A;
+
+function foo($obj){
+        // ($obj) = ($e) = <id>
+    $obj->foo = 2;
+}
+
+foo($e);
+echo $e->foo. "\n";
 ?>
 
 
-<?php  //Пример #3 Использование static:: в нестатическом контексте
-class A{
-    private function foo(){
-        echo "success!\n";
-    }
-
-    public function test(){
-        $this->foo();
-        static::foo();
-    }
-}
-
-class B extends A{
-     /* foo() будет скопирован в В, следовательно его область действия по прежнему А,
-      и вызов будет успешным */
-}
-
-class C extends A{
-    private function foo(){
-                /* исходный метод заменён; область действия нового метода - С */
-    }
-}
-
-$b = new B();
-$b->test();
-$c = new C();
-$c->test();   // потерпит ошибку
-?>
-
-
-<?php  //Пример #4 Перенаправленные и неперенаправленные вызовы
-class A {
-    public static function foo(){
-        static::who();
-    }
-
-    public static function who(){
-        echo __CLASS__ . "\n";
-    }
-}
-
-class B extends A{
-    public static function test(){
-        A::foo();
-        parent::foo();
-        self::foo();
-    }
-
-    public static function who(){
-        echo __CLASS__ . "\n";
-    }
-}
-
-class C extends B{
-    public static function who(){
-        echo __CLASS__ ."\n";
-    }
-}
-
-C::test();
-?>
-<!-- https://www.php.net/manual/ru/language.oop5.late-static-bindings.php -->
+<!-- https://www.php.net/manual/ru/language.oop5.references.php -->
 </body> 
 </html>

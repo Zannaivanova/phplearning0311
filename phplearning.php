d2c7ad3b1234c573d63dc460ee46373c03844532
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-04 08:31:34 +0200

    Описание нескольких пространств имён в одном файле

--- phplearning.php
+++ phplearning.php
@@ -3,19 +3,76 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Определение подпространств имён</title>
+	<title>Описание нескольких пространств имён в одном файле</title>
 </head>
 <body>
 
-<?php //Пример #1 Определение пространства имён с иерархией
-namespace MyProject\Sub\Level;
+<?php  //Пример #1 Описание нескольких пространств имён, простой синтаксис
+namespace MyProject;
 
 const CONNECT_OK = 1;
 class Connection {/*...*/}
 function connect(){/*...*/}
 
+namespace AnotherProject;
+
+const CONNECT_OK = 1;
+class Connection {/*...*/}
+function connect(){/*...*/}
+?>
+
+
+<?php //Пример #2 Описание нескольких пространств имён, синтаксис со скобками
+namespace MyProject{
+
+const CONNECT_OK = 1;
+class Connection {/*...*/}
+function connect(){/*...*/}
+ }
+
+ namespace AnotherProject{
+
+const CONNECT_OK = 1;
+class Connection {/*...*/}
+function connect(){/*...*/}
+ }
  ?>
 
-<!-- https://www.php.net/manual/ru/language.namespaces.nested.php -->
+
+<?php  //Пример #3 Описание глобального и обычного пространства имён в одном файле
+namespace MyProject{
+
+    const CONNECT_OK = 1;
+class Connection {/*...*/}
+function connect(){/*...*/}
+}
+
+namespace{
+    session_start();
+    $a = MyProject\connect();
+    echo MyProject\Connection::start();
+}
+?>
+
+<?php  //Пример #4 Описание глобального и обычного пространства имён в одном файле
+
+declare(encoding = 'UTF-8');
+namespace MyProject{
+
+    const CONNECT_OK = 1;
+class Connection {/*...*/}
+function connect(){/*...*/}
+}
+
+namespace{
+    session_start();
+    $a = MyProject\connect();
+    echo MyProject\Connection::start();
+}
+?>
+
+
+
+<!-- https://www.php.net/manual/ru/language.namespaces.definitionmultiple.php -->
 </body> 
 </html>

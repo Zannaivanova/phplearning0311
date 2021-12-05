6b5b4cb9e510b14dbffac167ba24f4db2b25e984
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-05 18:10:20 +0200

    Использование пространства имён: основы

--- phplearning.php
+++ phplearning.php
@@ -1,78 +1,67 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Описание нескольких пространств имён в одном файле</title>
+	<title>Использование пространства имён: основы </title>
 </head>
 <body>
+<?php 
+namespace Foo\Bar\subnamespace;
 
-<?php  //Пример #1 Описание нескольких пространств имён, простой синтаксис
-namespace MyProject;
-
-const CONNECT_OK = 1;
-class Connection {/*...*/}
-function connect(){/*...*/}
-
-namespace AnotherProject;
-
-const CONNECT_OK = 1;
-class Connection {/*...*/}
-function connect(){/*...*/}
-?>
-
-
-<?php //Пример #2 Описание нескольких пространств имён, синтаксис со скобками
-namespace MyProject{
-
-const CONNECT_OK = 1;
-class Connection {/*...*/}
-function connect(){/*...*/}
+const Foo = 1;
+function foo(){}
+ class foo {
+    static function staticmethod(){
+    }
  }
-
- namespace AnotherProject{
-
-const CONNECT_OK = 1;
-class Connection {/*...*/}
-function connect(){/*...*/}
- }
- ?>
-
-
-<?php  //Пример #3 Описание глобального и обычного пространства имён в одном файле
-namespace MyProject{
-
-    const CONNECT_OK = 1;
-class Connection {/*...*/}
-function connect(){/*...*/}
-}
-
-namespace{
-    session_start();
-    $a = MyProject\connect();
-    echo MyProject\Connection::start();
-}
 ?>
 
-<?php  //Пример #4 Описание глобального и обычного пространства имён в одном файле
+<?php  
+namespace Foo\Bar;
+include 'file1.php';
 
-declare(encoding = 'UTF-8');
-namespace MyProject{
-
-    const CONNECT_OK = 1;
-class Connection {/*...*/}
-function connect(){/*...*/}
+const FOO = 2;
+function foo(){}
+class foo{
+    static function staticmethod(){}
 }
 
-namespace{
-    session_start();
-    $a = MyProject\connect();
-    echo MyProject\Connection::start();
-}
+foo();
+foo::staticmethod();
+echo FOO;
+
+subnamespace\foo();
+subnamespace\foo::staticmethod();
+
+echo subnamespace\FOO;
+\FOO\Bar\foo();
+\FOO\Bar\::staticmethod();
+echo \FOO\Baar\Foo;
 ?>
 
 
+<?php //Пример #1 Доступ к глобальным классам, функциям и константам из пространства имён 
+namespace Foo;
 
-<!-- https://www.php.net/manual/ru/language.namespaces.definitionmultiple.php -->
+function strlen(){}
+    const INI_ALL = 3;
+    class Exeption{
+    }
+
+    $a = \strlen(){}
+    const INI_ALL = 3;
+    class Exeption{}
+
+    $a = \strlen('hi');
+    $b = \INI_ALL;
+    $c = new\Exeption('error');
+
+?>
+
+
+
+<!-- https://www.php.net/manual/ru/language.namespaces.basics.php -->
 </body> 
 </html>

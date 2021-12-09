7c39f99506c379ef90450acd7f8ca2a4c58b94a4
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-09 06:49:19 +0200

    Использование пространств имён: импорт/создание псевдонима имени

--- phplearning.php
+++ phplearning.php
@@ -4,56 +4,91 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Ключевое слово namespace и константа __NAMESPACE__ </title>
+	<title>Использование пространств имён: импорт/создание псевдонима имени</title>
 </head>
 <body>
 
-<?php  //Пример #1 Пример использование константы __NAMESPACE__ в коде с пространством имён
-namespace MyProject;
+<?php //Пример #1 импорт/создание псевдонима имени с помощью оператора use
+namespace foo;
+use My\Full\Classname as Another;
 
-echo '"', __NAMESPACE__, '"';
+use My\Full\NSame;
+
+use ArrayObject;
+
+use function My\Full\functionName;
+
+use function My\Full\functionName as func;
+
+
+use const My\Full\CONSTANT;
+
+$obj = new namespace\Another;
+$obj = new Another;
+NSname\subns\func();
+$a = new ArrayObject(array(1));
+
+func();
+echo CONSTANT;
+ ?>
+
+
+<?php  //Пример #2 импорт/создание псевдонима имени с помощью оператора use, комбинирование нескольких операторов use
+use My\Full\Classname as Another, My\Full\NSname;
+
+$obj = new Another;
+NSname\subns\func();
 ?>
 
 
-<?php //Пример #2 Пример использование константы __NAMESPACE__ в глобальном пространстве
-echo '"', __NAMESPACE__, '"';
+<?php //Пример #3 Импорт и динамические имена
+use My\Full\
+Classname as Another, My\Full\NSname;
+
+$obj = new Another;
+$a = 'Another';
+$obj = new $a;
  ?>
 
+<?php //Пример #4 Импортирование и абсолютные имена
+use My\Full\Classname as Another, My\Full\NSname;
 
- <?php  //Пример #3 использование константы __NAMESPACE__ для динамического конструирования имени
-namespace MyProject;
+$obj = new Another;
+$obj = new \Another;
+$obj = new Another\thing;
+$obj = new \Another\thing;
+ ?>
+ 
 
-function get($classname){
-$a = __NAMESPACE__ . '\\' . $classname;
-return new $a;
-}
+ <?php  //Пример #4 Импортирование и абсолютные имена
+ namespace Languages;
+
+ function toGreenlandic(){
+ 	use Languages\Danish;
+ }
  ?>
 
- <?php  //Пример #4 Оператор namespace, внутри пространства имён
- namespace MyProject;
+<?php  //Описание группирования в одном операторе use ¶
+use some\namespace\ClassA;
+use some\namespace\ClassB;
+use some\namespace\ClassC as C;
 
- use blah\blah as mine;
+use function some\namespace\fn_a;
+use function some\namespace\fn_b;
+use function some\namespace\fn_c;
 
- blah\mine();
- namespace\blah\mine();
+use const some\namespace\ConstA;
+use const some\namespace\ConstB;
+use const some\namespace\ConstC;
 
- namespace\func();
- namespace\sub\func();
- namespace\cname::method();
- $a = new namespace\sub\cname();
- $b = namespace\CONSTANT;
- ?>
+use some\namespace\{ClassA, ClassB, ClassC as C};
+use function some\namespace\{fn_a,fn_b,fn_c};
+use const some\namespace\{ConstA, ConstB, ConstC};
 
 
-<?php //Пример #5 Оператор namespace в глобальном коде
-namespace\func();
-namespace\sub\func();
-namespace\cname::method();
-$a = new namespace\sub\cname();
-$b = namespace\CONSTANT;
+?>
 
- ?>
 
-<!-- https://www.php.net/manual/ru/language.namespaces.nsconstants.php -->
+<!-- https://www.php.net/manual/ru/language.namespaces.importing.php -->
 </body> 
 </html>

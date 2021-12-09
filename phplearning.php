b42d42bda7106265acd7668456a5ed1d41dc02db
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-09 07:51:41 +0200

    Использование пространств имён: переход к глобальной функции/константе

--- phplearning.php
+++ phplearning.php
@@ -4,18 +4,40 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Глобальное пространство</title>
+	<title>Использование пространств имён: переход к глобальной функции/константе </title>
 </head>
 <body>
+<?php //Пример #1 Доступ к глобальным классам внутри пространства имён
+namespace A\B\C;
+class Exeption extends \Exeption {}
 
-<?php //Пример #1 Использование глобального пространства и его задание
+$a = new Exeption('hi');
+$b = new \Exeption('hi');
+
+$c = new ArrayObject;
+?>
+
+
+<?php//Пример #2 Необходимость прибегнуть к глобальным функциям/константам внутри пространства имён
+
 namespace A\B\C;
 
-function fopen(){
-$f = \fopen(...);
-return $f;	
- 
-}?>
-<!-- https://www.php.net/manual/ru/language.namespaces.global.php -->
+const E_ERROR = 45;
+function strlen($str){
+	return \strlen($strlen) -1;
+} 
+
+
+echo E_ERROR, "\n";
+echo INI_ALL, "\n";
+
+echo strlen('hi'), "\n";
+if (is_array('hi')){
+	echo "это массив \n";
+}
+
+ ?>
+
+<!-- https://www.php.net/manual/ru/language.namespaces.fallback.php -->
 </body> 
 </html>

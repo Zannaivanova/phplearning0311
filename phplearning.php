4cc0835e050cd62f65300a6925ece9e84ded8712
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-10 10:51:51 +0200

    Генераторы

--- phplearning.php
+++ phplearning.php
@@ -4,24 +4,39 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Fibers</title>
+	<title>Генераторы</title>
 </head>
 <body>
 
-<?php  //Пример #1 Basic usage
-$fiber = new Fiber(function (): void {
-   $value = Fiber::suspend('fiber');
-   echo "Значение возобновлённого файбера: ", $value, PHP_EOL;
-});
+<?phpПример #1 Реализация range() как генератора
+function xrange($start, $limit, $step = 1){
+	if($start<= $limit){
+		if($step <=0){
+			throw new LogicException('Шаг должен быть положительным');
+		}
 
-$value = $fiber->start();
+		for ($i = $start; $i<=$limit; $i +=$step){
+			yield $i;}
+		} else {
+			if($step>=0){
+				throw new LogicException('Шаг должен быть отрицательным');
+			}
 
-echo "Значение приостановленного файбера: ", $value, PHP_EOL;
+			for ($i  = $start; $i>= $limit; $i +=$step){
+				yield $i;
+			
+		}
+	}
+}
 
-$fiber->resume('test');
-?>
+echo 'Нечетные однозначные числа с помощью xrange(): ';
+foreach (xrange(1,9,2) as $number){
+	echo "$number";
+}
 
-<!-- https://www.php.net/manual/ru/language.fibers.php -->
+  ?>
+
+<!-- https://www.php.net/manual/ru/language.generators.php -->
 </body> 
 </html>
 

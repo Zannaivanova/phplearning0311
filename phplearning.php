52480121bf6eac0f487e46bba4ecd8ecf0bc65bf
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-11 18:10:21 +0200

    Интерфейс IteratorAggregate

--- phplearning.php
+++ phplearning.php
@@ -4,28 +4,37 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>ErrorException</title>
+	<title>Интерфейс IteratorAggregate </title>
 </head>
 <body>
 
-<?php  //Пример #1 Использование set_error_handler() для изменения сообщений об ошибках в ErrorException.
+<?php //Пример #1 Основы использования
+class myData implements IteratorAgregate {
+    public $property1 = "Первое обшедоступное свойство";
+    public $property2 = "Второе обшедоступное свойство";
+    public $property3 = "Третье обшедоступное свойство";
 
-function exeption_error_handler($severrity, $message, $file, $line){
-
-    if(! (error_reporting() & $severrity)){
-        return;
+    public function __construct(){
+        $this->property4 = "последнее свойство";
     }
-    throw new ErrorExeption(message, 0, $severity, $file, $line);
+
+    public function getIterator(){
+        return function getIterator(){
+            return new ArrayIterator($this);
+        }
+    }
+
+    $obj = new myData;
+
+    foreach($obj as $key=>$value){
+        var_dump($key, $value);
+        echo "\n";
+    }
 }
 
-set_error_handler("exeption_error_handler");
+ ?>
 
-strpos();
-
-?>
-
-
-<!-- https://www.php.net/manual/ru/class.errorexception.php -->
+<!-- https://www.php.net/manual/ru/class.iteratoraggregate.php -->
 </body> 
 </html>
 

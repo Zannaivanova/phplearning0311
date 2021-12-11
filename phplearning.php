0a3e5fb1c43117eaa45f0ce6c6c3115121423b5f
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-11 08:55:54 +0200

    Cсылки

--- phplearning.php
+++ phplearning.php
@@ -4,54 +4,168 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Объявление классов атрибутов</title>
+	<title>Cсылки</title>
 </head>
 <body>
 
-<?php  //Пример #1 Простой класс с атрибутом
-namespace Example;
+<?php  //Пример #1 Использование ссылок с неинициализированными переменными
+function foo(&$var){}
 
-use Attribute;
+foo($a);
 
-#[Attribute]
+$b = array();
+foo($b['b']);
+var_dump(array_key_exists('b', $b));
 
-class MyAttribute
-{
-
-}
+$c = new stdClass;
+foo($c->d);
+var_dump(property_exists($c, 'd'));
 ?>
 
 
-<?php  //Пример #2 Ограничение допустимых целей для использования атрибута
-namespace Example;
+<?php //Пример #2 Присвоение ссылок глобальным переменным внутри функции
+$var1 = "Пример переменной";
+$var2 = "";
 
-use Attribute;
+function global_references($use_globals){
+	global $var1, $var2;
+if (!$use_globals){
+	$var2 = &var1;
+} else {
+	$GLOBALS["var2"]=&$var1;
+ }
+}
+ 
+global_references(false);
+echo "значение var2: '$var2'\n";
+global_references(true);
+echo "значение var2: '$var2'\n";
 
-#[Attribute(Attribute::TARGET_METHOD | Attribute::TARGET_FUNCTION)]
-class MyAttribute
-{
+ ?>
+
+
+
+<?php  //Пример #3 Ссылки и foreach
+$ref = 0;
+$row = &ref;
+foreach (array(1,2,3) as $row){
 
 }
+
+echo $ref;
+?>
+
+<?php  
+$a = 1;
+$b = array(2,3);
+$arr = array(&$a, &$b[0], &$b[1]);
+$arr[0]++; $arr[1]++; $arr[2]++;
 ?>
 
 
-<?php  //Пример #3 Использование IS_REPEATABLE для разрешения использовать атрибут в объявлении несколько раз
-namespace Example;
+<?php  
+$a = 1;
+$b = &$a;
+$c = $b;
+$c = 7;
 
-use Attribute;
-
-#[Attribute(Attribute::TARGET_METHOD| Attribute::TARGET_FUNCTION| Attribute::IS_REPEATABLE)]
-
-class MyAttribute
-{
-
-}
-
+$arr = array(1);
+$a = &$arr[0];
+$arr2 = $arr;
+$arr2[0]++;
 ?>
 
 
+<?php  //Передача по ссылке 
+function foo(&$var){
+	$var++;
+}
 
-<!-- https://www.php.net/manual/ru/language.attributes.classes.php -->
+$a = 5;
+foo($a);
+?>
+
+
+<?php //ссылки не являются указателями
+function foo(&$var){
+	$var = & $GLOBALS["baz"];
+}
+
+foo($bar);
+ ?>
+
+ 
+ <?php//Передача по ссылке
+ function foo(&$var){
+ 	$var++;
+ }  
+
+ $a = 5;
+ foo($a);
+ ?>
+
+<?php  
+function foo(&$var){
+	$var++;
+}
+
+function &bar(){
+	$a = 5;
+	return $a;
+}
+foo(bar());
+?>
+
+
+<?php //Возврат по ссылке
+class foo{
+	public $value = 42;
+
+	public function &getValue(){
+		return $this->value;
+	}
+}
+
+$obj = new foo;
+$myValue = &$obj->getValue();
+$obj->value = 2;
+echo $myValue;
+ ?>
+
+
+ <?php  
+ function &collector(){
+ 	static $collection = array(;
+ 		return $collection;)
+ }
+
+ $collection = &collector();
+ $collection[] = 'foo';
+ ?>
+
+
+ <?php 
+ function &collector(){
+ 	static $colle tion = array();
+ 	return $collection;
+ }
+
+ array_push(collector(), 'foo');
+  ?>
+
+
+<?php  //Сброс переменных-ссылок
+$a = 1;
+$b = &a;
+unset($a);
+?>
+
+
+<?php //Ссылки global 
+$var = &$GLOBALS["var"];
+?>
+
+<!-- https://www.php.net/manual/ru/language.references.php -->
 </body> 
 </html>
 

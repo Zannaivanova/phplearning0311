faf459b4a112ed774eb1ae57d73fd9dbca359899
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-15 13:37:13 +0200

    Опции контекста CURL

--- phplearning.php
+++ phplearning.php
@@ -4,46 +4,30 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Опции контекста HTTP</title>
+	<title>Опции контекста CURL</title>
 </head>
 <body>
 
-<?php //Пример #1 Извлечь страницу и отправить данные методом POST
+<?php //Пример #1 Получает страницу и посылает POST-запрос
 $postdata = http_build_query(
 array(
-'var1'=>'некоторое содержимое';
-'var2'=>'doh'
+'var1'=>'некоторое содержание',
 ));
 
-$opts = array('http'=>
-array(
-'method' =>'POST',
-'header' => 'Content-type: application/x-www-form-urlencoded',
-'content' =>$posdata));
+$opts = array = array('http'=>
+	array(
+		'method'=>'POST',
+		'header'=>'Content-type: application/x-www-form-urlencoded',
+	    'content'=>$postdata)
+);
 
 $context = stream_context_create($opts);
 
 $result = file_get_contents('http://example.com/submit.php', false, $context);
+
  ?>
 
-<?php //Пример #2 Игнорировать переадресации, но извлечь заголовки и содержимое
-$url = "http://www.example.org/header.php";
-
-$opts = array('http'=>
-array(
-'method'=>'GET',
-'max_redirects' => '0',
-'ignore_errors' =>'1'));
-
-$context = stream_context_create($opts);
-$stream = fopen($url, 'r', false, $context);
-
-var_dump(stream_get_meta_data($stream));
-fclose($stream);
- ?>
-
-<!-- https://www.php.net/manual/ru/context.http.php -->
-
+<!-- https://www.php.net/manual/ru/context.curl.php -->
 </body> 
 </html>
 

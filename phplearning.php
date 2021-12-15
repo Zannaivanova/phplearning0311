c04ffea9f5493a4dde0aaa0ba13d2a3e859160d7
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-15 08:39:58 +0200

    Класс WeakMap

--- phplearning.php
+++ phplearning.php
@@ -4,21 +4,31 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Класс WeakReference</title>
+	<title>Класс WeakMap</title>
 </head>
 <body>
 
-<?php//Пример #1 Пример #1. Простое использование WeakReference
+<?php//Пример #1 Пример использования Weakmap
+$wm = new WeakMap();
 
-$obj = new stdClass;
-$weakref = WeakReference::create($obj);
-var_dump($weakref->get());
-unset($obj);
-var_dump($weakref->get());
- ?>
+$o = new StdClass;
 
- 
-<!-- https://www.php.net/manual/ru/class.weakreference.php -->
+class A {
+    public function __destruct() {
+        echo "Уничтожено!\n";
+    }
+}
+
+$wm[$o] = new A;
+
+var_dump(count($wm));
+echo "Сброс...\n";
+unset($o);
+echo "Готово\n";
+var_dump(count($wm)); 
+?>
+
+<!-- https://www.php.net/manual/ru/class.weakmap.php -->
 </body> 
 </html>
 

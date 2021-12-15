61ca73000b1aed0c7650536459516dee9ca71df2
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-15 08:15:47 +0200

    Класс WeakReference

--- phplearning.php
+++ phplearning.php
@@ -4,39 +4,21 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Интерфейс Serializable</title>
+	<title>Класс WeakReference</title>
 </head>
 <body>
 
-<?php  //Пример #1 Основы использования
-class obj implements Serializable {
-    private $data;
-    public function __construct(){
-        $this->data = "Мои закрытые данные";
-    }
-    public function serialize(){
-        return serialize($this->data);
-    }
-    public function unserialize($data){
-        $this->data = unserialize($data);
-    }
-    public function getData(){
-        return $this->data;
-    }
-}
+<?php//Пример #1 Пример #1. Простое использование WeakReference
 
-$obj = new obj;
-$ser = serialize($obj);
+$obj = new stdClass;
+$weakref = WeakReference::create($obj);
+var_dump($weakref->get());
+unset($obj);
+var_dump($weakref->get());
+ ?>
 
-var_dump($ser);
-
-$newobj = unserialize($ser);
-
-var_dump($newobj->getData());
-
-?>
-
-<!-- https://www.php.net/manual/ru/class.serializable.php -->
+ 
+<!-- https://www.php.net/manual/ru/class.weakreference.php -->
 </body> 
 </html>
 

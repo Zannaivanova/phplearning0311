0019228a94d443a82b515c777798771d8637d9c0
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-16 17:55:30 +0200

    Безопасность файловой системы

--- phplearning.php
+++ phplearning.php
@@ -4,58 +4,91 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Безопасность. Сессии. Класс SessionHandlerInterface</title>
+	<title>Безопасность файловой системы</title>
 </head>
 <body>
 
-	<?php//Пример #1 Пример использования SessionHandlerInterface 
-class MySessionHandler implements SessionHandlerInterface {
-	private $savePath;
-	public function open($savePath, $sessionName):bool{
-		$this->savePath = $savePath;
-		if(!is_dir($this->$savePath)){
-			mkdir($this->savePath, 0777);
-		}
-		return true;
-	}
+<?php //Пример #1 Недостаточная проверка внешних данных ведёт к...
+$username = $_POST['user_submitted_name'];
+$userlife = $_POST['user_submited_filename'];
+$homedir = "/home/$username";
 
-	public function close():bool{
-		return true;
-	}
+unlink("$homedir/$userfile");
 
-	#[ReturnTypeWillChange]
-	public function read($id){
-		return (string)@file_get_contents("$this->savePath/sess_$id");
-	}
+echo "Файл был удален";
+ ?>
+	
+<?php //Пример #2 ... атаке на файловую систему
+$username = $_POST['user_submitted_name'];
+$userfile = $_POST['user_submitted_filename'];
+$homedir = "/home/$username";
 
-	public function write($id, $data): bool{
-		return file_put_contants("$this->savePath/sess_$id", $data) ===false?false:true;
-	}
-	public function destroy ($id):bool{
-		$file = "$this->savePath/sess_$id";
-		if (file_exists($file)){
-			unlink($file);
-		}
-		return true;
-	}
+unlink("$homedir/$userfile");
 
-	#[ReturnTypeWillChange]
-	public function gc($maxlifetime){
-		foreach (glob("$this->savePath/sess_*")as $file){
-			if (filetime($file)+$maxlifetime<time()&&file_exists($file)){
-				unlink($file);
-			}
-		}
-		return true;
-	}
+echo "Файл был удален" 
+?>
+
+
+<?php//Пример #3 Более безопасная проверка имени файла
+$username = $_SERVER['REMOTE_USER'];
+$userfile = basename($_POST['user_submitted_filename']);
+$homedir = "/home/$username";
+
+$filepath = "$homedir/$userfile";
+
+if(file_exists($filepath) && unlink($filepath)){
+	$logstring = "$filepath удален";
+}  else {
+	$logstring = "Не удалось удалить $filepath\n";
 }
-$handler = new MySessionHandler();
-session_set_save_handler($handler, true);
-session_start();
-	 ?>
+$fp = fopen("/home/logging/filedelete.log", "a");
+fwrite($fp, $logstring);
+fclose($fp);
+
+echo htmlentities($logstring, ENT_QUOTES);
+?>
+
+<?php//Пример #4 Более строгая проверка имени файла
+$username = $_SERVER['REMOTE_USER'];
+$userfile = $_POST['user_submitted_filename'];
+$homedir = "/home/$username";
+
+$filepath = "$homedir/$userfile";
+
+if(!ctype_alnum($username)|| !preg_match('/^(?:[a-z0-9_-]|\.(?!\.))+$/iD', $userfile)) {
+	die("Непраильное имя пользователя или файл");
+}
+
+ ?>
 
 
-<!-- https://www.php.net/manual/ru/class.sessionhandlerinterface.php -->
+<?php //Пример #1 Скрипт, уязвимый к нулевому байту
+$file = $_GET['file'];
+if (file_exists('/home/wwwrun/'.$file.'.php')){
+	include '/home/wwwrun/'.$file.'.php';
+}
+
+ ?>
+
+
+<?php //Пример #2 Корректная проверка входных данных
+$file = $_GET['file'];
+
+switch($file){
+	case 'main':
+	case 'foo':
+	case 'bar':
+	 include '/home/wwwrun/include/'.$file.'.php';
+	 break;
+	 default:
+	 include '/home/wwwrun/include/main.php';
+}
+
+ ?>
+
+
+ 
+<!-- https://www.php.net/manual/ru/security.filesystem.php -->
 </body> 
 </html>
 

b64c3e678aa2d9a0b8bdf734c7f25d7a722142de
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-17 13:26:40 +0200

    Поддержка метода PUT

--- phplearning.php
+++ phplearning.php
@@ -4,60 +4,24 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Загрузка файлов методом POST</title>
+	<title>Поддержка метода PUT</title>
 </head>
 <body>
 
-<!-- Пример #1 Форма для загрузки файлов -->
-<!-- Тип кодирования данных, enctype, ДОЛЖЕН БЫТЬ указан ИМЕННО так -->
-<form enctype="multipart/form-data" action="__URL__" method="POST">
-    <!-- Поле MAX_FILE_SIZE должно быть указано до поля загрузки файла -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="30000" />
-    <!-- Название элемента input определяет имя в массиве $_FILES -->
-    Отправить этот файл: <input name="userfile" type="file" />
-    <input type="submit" value="Отправить файл" />
-</form>
+<?php //Пример #1 Сохранение файлов, отправленных через HTTP PUT
+$putdata = fopen("php://input", "r");
 
+$fp = fopen("myputfile.ext", "w");
 
-<?php //Пример #2 Проверка загружаемых на сервер файлов
-$uploaddir = '/var/www/uploads/';
-$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+while ($data = fread($putdata, 1024))
+fwrite($fp, $data);
 
-echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)){
-	echo "Файл корректен и был успешно загружен.";
-} else{
-	echo "Возможна атака с помощью файловой загрузки";
-}
+fclose($fp);
+fclose($putdata);
 
-echo 'некотроая отладочная информация: ';
-print_r($_FILES);
-
-print "</pre>";
  ?>
 
- <!-- Пример #3 Загрузка массива файлов -->
- <form action="" method="post" enctype="multipart/form-data">
-<p>Изображения:
-<input type="file" name="pictures[]" />
-<input type="file" name="pictures[]" />
-<input type="file" name="pictures[]" />
-<input type="submit" value="Отправить" />
-</p>
-</form>
-<?php 
-foreach ($_FILES["pictures"]["error"] as $key=>$error){
-	if ($error ==UPLOAD_ERR_OK){
-		$tmp_name = $_FILES["pictures"]["tmp_name"][$key];
-		$name = basename($_FILES["pictures"]["name"][$key]);
-		move_uploaded_file($tmp_name, "data/$name");
-	}
-}
- ?>
-
-
-
-<!-- https://www.php.net/manual/ru/features.file-upload.post-method.php -->
+<!-- https://www.php.net/manual/ru/features.file-upload.put-method.php -->
 </body> 
 </html>
 

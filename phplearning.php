3d867f898e6dd91c00755ead1e04c9b97b0a40a4
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-17 05:31:56 +0200

    Сокрытие PHP

--- phplearning.php
+++ phplearning.php
@@ -4,26 +4,29 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Данные, введённые пользователем</title>
+	<title>Сокрытие PHP</title>
 </head>
 <body>
+<!-- в .htaccess файлах, так и конфигурационном файле Apache -->
 
-<?php //Пример #1 Потенциально опасное использование переменных
-
-// удалить файлы из домашней директории пользователя...
-// а может, из ещё какой-нибудь?
-unlink ($evil_var);
-
-// записать в лог-файл выполняемое действие...
-// а может быть в /etc/passwd?
-fwrite($fp, $evil_var);
-
-// выполнение тривиальных действий... или rm -rf *?
-system($evil_var);
-exec($evil_val);
+<?php //Пример #1 Маскировка PHP под другие языки программирования
+# Теперь PHP-скрипты могут иметь те же расширения, что и другие языки программирования
+AddType application/x-httpd-php .asp .py .pl
  ?>
 
-<!-- https://www.php.net/manual/ru/security.variables.php -->
+
+<?php //Пример #2 Использование неизвестных расширений для PHP-скриптов
+# Теперь PHP-скрипты могут иметь неизвестные типы файлов
+AddType application/x-httpd-php .bop .foo .133t
+ ?>
+
+
+ <?php //Пример #3 Маскировка PHP-файлов под HTML
+# Теперь PHP-скрипты выглядят как обыкновенный HTML
+AddType application/x-httpd-php .htm .html
+  ?>
+
+<!-- https://www.php.net/manual/ru/security.hiding.php -->
 </body> 
 </html>
 

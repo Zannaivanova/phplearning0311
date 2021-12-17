419f2f494dd1237737a31f982eb7647a7a03b04b
Zannaivanova <Zannaivanova@tutanota.com>
2021-12-17 05:23:06 +0200

    Данные, введённые пользователем

--- phplearning.php
+++ phplearning.php
@@ -4,30 +4,26 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Сообщения об ошибках</title>
+	<title>Данные, введённые пользователем</title>
 </head>
 <body>
 
+<?php //Пример #1 Потенциально опасное использование переменных
 
-<!-- Пример #2 Использование стандартных отладочных переменных -->
-    <form method="post" action="attacktarget?errors=Y&amp;showerrors=1&amp;debug=1">
-    <input type="hidden" name="errors" value="Y" />
-    <input type="hidden" name="showerrors" value="1" />
-    <input type="hidden" name="debug" value="1" />
-    </form>
+// удалить файлы из домашней директории пользователя...
+// а может, из ещё какой-нибудь?
+unlink ($evil_var);
 
+// записать в лог-файл выполняемое действие...
+// а может быть в /etc/passwd?
+fwrite($fp, $evil_var);
 
-<?php//Пример #3 Поиск потенциально опасных переменных при помощи E_ALL
-if($username){
-	$good_login = 1;
-} 
-if ($good_ligin == 1){
-	readfile("/highly/sensitive/data/index.html");
-}
-?>
+// выполнение тривиальных действий... или rm -rf *?
+system($evil_var);
+exec($evil_val);
+ ?>
 
-
-<!-- https://www.php.net/manual/ru/security.errors.php -->
+<!-- https://www.php.net/manual/ru/security.variables.php -->
 </body> 
 </html>
 

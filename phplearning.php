a8a55ad01041ecc154c4dd5a0b24b492ded1124f
Zannaivanova <Zannaivanova@tutanota.com>
2022-01-02 08:04:49 +0200

    Windows Cache для PHP

--- phplearning.php
+++ phplearning.php
@@ -3,314 +3,222 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Operations для Zend</title>    
+    <title>Windows Cache для PHP</title>    
 </head>
 <body>
-    <!-- uopz_add_function — Добавляет несуществующую функцию или метод -->
-<?php //Пример #1 Простое использование uopz_add_function()
-uopz_add_function('foo', function(){ echo 'bar';});
-foo();
- ?>
-    <!-- uopz_allow_exit — Позволяет управлять отключённым опкодом exit -->
-<?php //Пример #1 Пример использования uopz_allow_exit()
-exit(1);
-echo 1;
-uopz_allow_exit(true);
-exit(2);
-echo
- ?>
-    <!-- uopz_backup — Резервирует функцию -->
-    <!-- uopz_compose — Составить класс -->
-    <!-- uopz_copy — Скопировать функцию -->
-    <!-- uopz_del_function — Удаляет ранее добавленную функцию или метод -->
-<?php //Пример #1 Простое использование uopz_del_function()
-uopz_zdd_function('foo', function() {echo 'bar';});
-var_dump(function_exists('foo'));
-uopz_del_function('foo');
-var_dump(function_exists('foo'));
+     <!-- wincache_fcache_fileinfo — Получает информацию о файлах, закешированных в файловом кеше -->
+<?php //Пример #1 Пример использования wincache_fcache_fileinfo()
+print_r(wincache_fcache_fileinfo());
  ?>
 
-    <!-- uopz_delete — Удалить функцию -->
-    <!-- uopz_extend — Расширить класс во время выполнения -->
-<?php //Пример #1 Пример использования uopz_extend()
-class A {}
-class B {}
 
-uopz_extend(A::class, B::class);
-
-var_dump(class_parents(A::class));
+    <!-- wincache_fcache_meminfo — Получает информацию об использовании памяти файлового кеша -->
+<?php //Пример #1 Пример использования wincache_fcache_meminfo()
+print)r(wincache_fcache_meminfo());
  ?>
 
-    <!-- uopz_flags — Получить или установить флаги для функции или класса -->
-<?php //Пример #1 Пример использования uopz_flags()
-class Test{
-    public function method(){
-        return __CLASS__;
-    }
+    <!-- wincache_lock — Получает эксклюзивную блокировку для данного ключа -->
+<?php //Пример #1 Пример использования wincache_lock() 
+$fp = fopen("/tmp/lock.txt", "r+");
+if (wincache_lock("lock_txt_lock")){
+ftruncate($fp, 0);
+fwrite($fp, "Напиши что-нибудь здесь");
+wincache_unlock("lock_txt_lock");
+} else {
+    echo "не удалось получить блокировку";
 }
+fclose($fp);
+?>
 
-$flags = uopz_flags("Test", "method");
+    <!-- wincache_ocache_fileinfo — Получает информацию о файлах, закешированных в кеше опкодов -->
+    <!-- wincache_ocache_meminfo — Получает информацию об использовании кеш-памяти опкодов -->
 
-var_dump((bool)(uopz_flags("Test", "method")& ZEND_ACC_PRIVATE));
-var_dump((bool)(uopz_flags("Test", "method")& ZEND_ACC_STATIC));
+    <!-- wincache_refresh_if_changed — Обновляет записи кеша для закешированных файлов -->
+<?php //Пример #1 Пример использования wincache_refresh_if_changed()
+$filename = 'C:\inetpub\wwwroot\config.php';
+$handle = fopen($filename, 'w+');
+if ($handle === FALSE) die('Failed to open file '.$filename.' for writing');
+fwrite($handle, '<?php $setting=something; ?>');
+fclose($handle);
+wincache_refresh_if_changed(array($filename));
+?>
 
-var_dump(uopz_flags("Test", "method", $flags|ZEND_ACC_STATIC|ZEND_ACC_PRIVATE));
-
-var_dump((bool)(uopz_flags("Test", "method")&ZEND_ACC_PRIVATE));
-var_dump((bool)(uopz_flags("Test", "method")& ZEND_ACC_STATIC));
+    <!-- wincache_rplist_fileinfo — Получает информацию о разрешении кеша пути к файлу разрешения -->
+    <pre>
+<?php //Пример #1 Пример использования wincache_rplist_fileinfo()
+print_r(wincache_rplist_fileinfo());
  ?>
-
-    <!-- uopz_function — Создаёт функцию во время выполнения -->
-    <!-- uopz_get_exit_status — Получить последний установленный статус выхода -->
-<?php //Пример #1 Пример использования uopz_get_exit_status()
-exit(123);
-echo uopz_get_exit_status();
+</pre>
+    <!-- wincache_rplist_meminfo — Получает информацию об использовании памяти с помощью кеша пути к файлу разрешения -->
+<pre>
+<?php //Пример #1 Пример использования wincache_rplist_meminfo()
+print_r(wincache_rplist_meminfo());
  ?>
+</pre>
 
-    <!-- uopz_get_hook — Получает ранее установленный обработчик на функцию или метод -->
-<?php //Пример #1 Простое использование uopz_get_hook()
-function foo(){
-    echo 'foo';
+
+    <!-- wincache_scache_info — Получает информацию о файлах, закешированных в кеше сессии -->
+<pre>
+<?php//Пример #1 Пример использования wincache_scache_info()
+print_r(wincache_scache_info());
+?>
+</pre>
+
+    <!-- wincache_scache_meminfo — Получает информацию об использовании кеш-памяти сессии -->
+<pre>
+<?php//Пример #1 Пример использования wincache_scache_meminfo()
+print_r(wincache_scache_meminfo());
+?>
+</pre>
+
+    <!-- wincache_ucache_add — Добавляет переменную в пользовательский кеш, только если переменная ещё не существует в кеше -->
+
+<?php//Пример #1 Пример использования wincache_ucache_add() с key в виде строки
+$bar = 'BAR';
+var_dump(wincache_ucache_add('foo', $bar));
+var_dump(wincache_ucache_add('foo', $bar));
+var_dump(wincache_ucache_get('foo'));
+?>
+
+
+
+<?php//Пример #2 Пример использования wincache_ucache_add() с key в виде массива
+$colors_array = array('green' => '5', 'Blue' => '6', 'yellow' => '7', 'cyan' => '8');
+var_dump(wincache_ucache_add($colors_array));
+var_dump(wincache_ucache_add($colors_array));
+var_dump(wincache_ucache_get('Blue'));
+?>
+
+
+    <!-- wincache_ucache_cas — Сравнивает переменную со старым значением и присваивает ей новое значение -->
+<?php//Пример #1 Пример использования wincache_ucache_cas()
+wincache_ucache_set('counter', 2922);
+var_dump(wincache_ucache_cas('counter', 2922, 1));
+var_dump(wincache_ucache_get('counter'));
+?>
+
+    <!-- wincache_ucache_clear — Удаляет всё содержимое пользовательского кеша -->
+<?php//Пример #1 Пример использования wincache_ucache_clear()
+wincache_ucache_set('green', 1);
+wincache_ucache_set('red', 2);
+wincache_ucache_set('orange', 4);
+wincache_ucache_set('blue', 8);
+wincache_ucache_set('cyan', 16);
+$array1 = array('green', 'red', 'orange', 'blue', 'cyan');
+var_dump(wincache_ucache_get($array1));
+var_dump(wincache_ucache_clear());
+var_dump(wincache_ucache_get($array1));
+?>
+
+
+    <!-- wincache_ucache_dec — Уменьшает значение, связанное с ключом -->
+<?php//Пример #1 Пример использования wincache_ucache_dec()
+wincache_ucache_set('counter', 1);
+var_dump(wincache_ucache_dec('counter', 2923, $success));
+var_dump($success);
+?>
+
+
+    <!-- wincache_ucache_delete — Удаляет переменные из пользовательского кеша -->
+<?php//Пример #1 Пример использования wincache_ucache_delete() с key в виде строки
+wincache_ucache_set('foo', 'bar');
+var_dump(wincache_ucache_delete('foo'));
+var_dump(wincache_ucache_exists('foo'));
+?>
+
+
+<?php//Пример #2 Пример использования wincache_ucache_delete() с key в виде массива
+$array1 = array('green' => '5', 'blue' => '6', 'yellow' => '7', 'cyan' => '8');
+wincache_ucache_set($array1);
+$array2 = array('green', 'blue', 'yellow', 'cyan');
+var_dump(wincache_ucache_delete($array2));
+?>
+
+
+<?php//Пример #3 Пример использования wincache_ucache_delete() с key в виде массива, из которого нельзя удалить некоторые элементы
+$array1 = array('green' => '5', 'blue' => '6', 'yellow' => '7', 'cyan' => '8');
+wincache_ucache_set($array1);
+$array2 = array('orange', 'red', 'yellow', 'cyan');
+var_dump(wincache_ucache_delete($array2));
+?>
+
+    <!-- wincache_ucache_exists — Проверяет, существует ли переменная в пользовательском кеше -->
+<?phpПример #1 Пример использования wincache_ucache_exists()
+if (!wincache_ucache_exists('green'))
+    wincache_ucache_set('green', 1);
+var_dump(wincache_ucache_exists('green'));
+?>
+
+
+    <!-- wincache_ucache_get — Получает переменную, хранящуюся в пользовательском кеше -->
+<?php//Пример #1 wincache_ucache_get() с key в виде строки
+wincache_ucache_add('color', 'blue');
+var_dump(wincache_ucache_get('color', $success));
+var_dump($success);
+?>
+
+
+<?php//Пример #2 wincache_ucache_get() с key в виде массива
+$array1 = array('green' => '5', 'Blue' => '6', 'yellow' => '7', 'cyan' => '8');
+wincache_ucache_set($array1);
+$array2 = array('green', 'Blue', 'yellow', 'cyan');
+var_dump(wincache_ucache_get($array2, $success));
+var_dump($success);
+?>
+
+    <!-- wincache_ucache_inc — Увеличивает значение, связанное с ключом -->
+<?phpПример #1 Пример использования wincache_ucache_inc()
+wincache_ucache_set('counter', 1);
+var_dump(wincache_ucache_inc('counter', 2921, $success));
+var_dump($success);
+?>
+
+
+    <!-- wincache_ucache_info — Получает информацию о данных, хранящихся в пользовательском кеше -->
+<?php//Пример #1 Пример использования wincache_ucache_info()
+wincache_ucache_get('green');
+wincache_ucache_set('green', 2922);
+wincache_ucache_get('green');
+wincache_ucache_get('green');
+wincache_ucache_get('green');
+print_r(wincache_ucache_info());
+?>
+
+
+    <!-- wincache_ucache_meminfo — Получает информацию об использовании памяти пользовательского кеша -->
+<pre>
+<?php//Пример #1 Пример использования wincache_ucache_meminfo()
+print_r(wincache_ucache_meminfo());
+?>
+</pre>
+
+
+    <!-- wincache_ucache_set — Добавляет переменную в пользовательский кеш и перезаписывает переменную, если она уже существует в кеше -->
+<?php//Пример #1 Пример использования wincache_ucache_set() с key в виде строки
+$bar = 'BAR';
+var_dump(wincache_ucache_set('foo', $bar));
+var_dump(wincache_ucache_get('foo'));
+$bar1 = 'BAR1';
+var_dump(wincache_ucache_set('foo', $bar1));
+var_dump(wincache_ucache_get('foo'));
+?>
+
+    <!-- wincache_unlock — Снимает эксклюзивную блокировку данного ключа -->
+
+<?php//Пример #1 Пример использования wincache_unlock()
+$fp = fopen("/tmp/lock.txt", "r+");
+if (wincache_lock(“lock_txt_lock”)) { // получить эксклюзивную блокировку
+    ftruncate($fp, 0); // обрезать файл
+    fwrite($fp, "Напишите что-нибудь здесь\n");
+    wincache_unlock(“lock_txt_lock”); // снять блокировку
+} else {
+    echo "Не удалось получить блокировку!";
 }
-uopz_set_hook('foo', function() {echo 'bar';});
-var_dump(uopz_get_hook('foo'));
- ?>
-
-    <!-- uopz_get_mock — Получить текущий имитатор (mock) для класса -->
-<?php //Пример #1 Пример использования uopz_get_mock()
-calss A {
-    public static function who(){
-        echo "A";
-    }
-}
-
-class mockA {
-    public static function who(){
-        echo "mockA";
-    }
-}
-
-uopz_set_mock(A::class, mockA::class);
-echo uopz_get_mock(A::class);
- ?>
+fclose($fp);
+?>
 
 
-    <!-- uopz_get_property — Получает значение класса или свойство экземпляра -->
-<?php //Пример #1 Простое использование uopz_get_property()
-class Foo{
-    private static $staticBar = 10;
-    private $bar = 100;
-}
-
-$foo = new Foo;
-var_dump(uopz_get_property('Foo', 'staticBar'));
-var_dump(uopz_get_property($foo, 'bar'));
- ?>
-
-    <!-- uopz_get_return — Получает предыдущее установленное возвращаемое значение для функции -->
-<?php //Пример #1 Пример использования uopz_get_return()
-uopz_set_return("strlen", 42);
-echo uopz_set_return("strlen");
- ?>
-
-    <!-- uopz_get_static — Возвращает статические переменные из области видимости функции или метода -->
-<?php //Пример #1 Пример использования uopz_get_return()
-function foo(){
-    static $bar = 'baz';
-}
-var_dump(uopz_get_static('foo'));
- ?>
-
-    <!-- uopz_implement — Реализует интерфейс во время выполнения -->
-<?php //Пример #1 Пример использования uopz_implement()
-interface myInterface {}
-
-class myClass {}
-
-uopz_implement(myClass::class, myInterface::class);
-
-var_dump(class_implements(myClass::class));
- ?>
-
-    <!-- uopz_overload — Перегрузить опкод VM -->
-    <!-- uopz_redefine — Переопределить константу -->
-<?php //Пример #1 Пример использования uopz_redefine()
-define("MY", 100);
-
-uopz_redefine("MY", 1000);
-
-echo MY;
- ?>
-
-    <!-- uopz_rename — Переименовать функцию во время выполнения -->
-    <!-- uopz_restore — Восстановить ранее зарезервированную функцию -->
-    <!-- uopz_set_hook — Устанавливает обработчик для выполнения при вызове функции или метода -->
-<?php //Пример #1 Простое использование uopz_set_hook()
-function foo(){
-    echo 'foo';
-}
-uopz_set_hook('foo', function (){echo 'bar';});
-foo();
- ?>
-
-
-    <!-- uopz_set_mock — Использовать имитатор вместо класса для новых объектов -->
-<?php //Пример #1 Пример использования uopz_set_mock()
-class A {
-    public function who(){
-        echo "A";
-    }
-}
-
-class mockA {
-    public function who(){
-        echo "mockA";
-    }
-}
-
-uopz_set_mock(A::class, mockA::class);
-(new A)->who();
- ?>
-
-
-<?php //Пример #2 Пример использования uopz_set_mock()
-class A{
-    public function who(){
-        echo "A";
-    }
-}
-
-uopz_set_mock(A::class, new class {
-    public function who(){
-        echo "mockA";
-    }
-});
-(new A)-> who();
- ?>
-
- <?php //Пример #3 uopz_set_mock() и статические члены класса
-class A {
-    public static function who(){
-        echo "A";
-    }
-}
-uopz_set_mock(A::class, new class{
-    const CON = 'mockA';
-    public static function who(){
-        echo "mockA";
-    }
-});
-echo A::CON, PHP_EOL;
-A::who();
-  ?>
-
-
-    <!-- uopz_set_property — Устанавливает значение существующего свойства класса или экземпляра -->
-
-<?php //Пример #1 Простое использование uopz_set_property()
-class FOO{
-    private static $staticBar;
-    private $bar;
-    public static function testStaticBar(){
-        return self::$staticBar;
-    }
-    public function testBar(){
-        return $this->bar;
-    }
-}
-$foo = new Foo;
-uopz_set_property('Foo', 'staticBar', 10);
-uopz_set_property($foo, 'bar', 100);
-var_dump(Foo::testStaticBar());
-var_dump($foo->testBar());
- ?>    
-    
-
-    <!-- uopz_set_return — Предоставить возвращаемое значение для существующей функции -->
-<?php //Пример #1 Пример использования uopz_set_return()
-uopz_set_return("strlen", 42);
-echo strlen("Banana");
- ?>
-
-<?php //Пример #2 Пример использования uopz_set_return()
-uopz_set_return("strlen", function($str){return strlen($str)*2;}, true);
-echo strlen("Banana");
- ?>
-
-
- <?php //Пример #3 Пример использования uopz_set_return() с классом
-class My {
-    public static function strlen($arg){
-        return strlen($arg);
-    }
-}
-
-uopz_set_return(My::class, "strlen", function($str){return strlen($str)*2;}, true);
-echo My::strlen("Banana");
-  ?>
-
-
-    <!-- uopz_set_static — Устанавливает статические переменные в области видимости функции или метода -->
-<?php //Пример #1 Простое использование uopz_set_static()
-function foo(){
-    static $bar = 'baz';
-    var_dump($bar);
-}
-uopz_set_static('foo', ['bar'=>'qux']);
-foo();
- ?>
-
-    <!-- uopz_undefine — Отменяет определение константы -->
-<?php //Пример #1 Пример использования uopz_undefine()
-define("MY", true);
-
-uopz_undefine("MY");
-
-ver_damp(defined("MY"));
- ?>
-
-    <!-- uopz_unset_hook — Удаляет ранее установленную функцию или метод -->
-<?php //Пример #1 Простое использование uopz_unset_hook()
-function foo(){
-    echo 'foo';
-}
-
-uopz_set_hook('foo', function (){echo 'bar';});
-foo();
-echo PHP_EOL;
-uopz_unset_hook('foo');
-foo();
- ?>
-
-    <!-- uopz_unset_mock — Удалить ранее установленный имитатор -->
-<?php //Пример #1 Пример использования uopz_unset_mock()
-class A{
-    public static function who(){
-        echo "A";
-    }
-}
-
-class mockA {
-    public static function who(){
-        echo "mockA";
-    }
-}
-
-uopz_set_mock(A::class, mockA::class);
-uopz_unset_mock(A::class);
-A::who();
- ?>
-
-
-    <!-- uopz_unset_return — Отменяет ранее установленное возвращаемое значение для функции -->
-<?php //Пример #1 Пример использования uopz_unset_return()
-uopz_set_return("strlen", 42);
-$len = strlen("Banana");
-uopz_unset_return("strlen");
-echo $len + strlen("Banana");
- ?>
-
-<!-- https://www.php.net/manual/ru/book.uopz.php -->
+<!-- https://www.php.net/manual/ru/book.wincache.php -->
 </body> 
 </html>
 

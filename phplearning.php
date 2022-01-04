b08b342f4f60350ea75962a8a3061f6427c45379
Zannaivanova <Zannaivanova@tutanota.com>
2022-01-04 16:45:17 +0200

    Radius

--- phplearning.php
+++ phplearning.php
@@ -3,53 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>xhprof Hierarchical Profiler</title>    
+    <title>RADIUS</title>    
 </head>
 <body>
- <?php //Пример #1 Пример использования xhprof_disable()
-xhprof_enable();
+<?php //Пример #1 Использование паролей CHAP
+$radth = radius_auth_open();
+radius_add_server($radh, $server, $port, $secret, 3, 3);
+radius_create_request($radh, RADIUS_ACCESS_REQUEST);
 
-$foo = strlen("foo bar");
+$challenge = mt_rand();
 
-$xhprof_data = xhprof_disable();
+$ident = 1;
 
-print_r($xhprof_data);
-  ?>    
+$cp = md5(pack('Ca*', $ident, $password.$challenge), true);
+radius_put_attr($radh, RADIUS_CHAP_PASSWORD, pack('C', $ident).$cp);
 
-
-<?php //Пример #1 Пример использования xhprof_disable()
-// 1. время исполнения + память + CPU; также игнорируем функции
-стандартной библиотеки
-xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
-
-// 2. время исполнения; игнорируем функции ignore call_user_func*
-xhprof_enable(
-    0,
-    array('ignored_functions' =>  array('call_user_func',
-                                        'call_user_func_array')));
-
-// 3. время исполнения + память; игнорируем функции call_user_func*
-xhprof_enable(
-    XHPROF_FLAGS_MEMORY,
-    array('ignored_functions' =>  array('call_user_func',
-                                        'call_user_func_array')));
-?>
-
-
-<?php //Пример #1 Пример использования xhprof_sample_disable()
-xhprof_sample_enable();
-
-for($i=0; $i<=10000; $i++){
-    $a = strlen($i);
-    $b = $i*$a;
-    $c = rand();
-}
-$xhprof_data = xhprof_sample_disable();
-print_r($xhprof_data);
+radius_put_attr($radh, RADIUS_CHAP_CHELLENGE, $challenge);
  ?>
 
-
-<!-- https://www.php.net/manual/ru/book.xhprof.php -->
+<!-- https://www.php.net/manual/ru/radius.constants.attributes.php -->
 </body> 
 </html>
 

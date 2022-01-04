961860cf38ead831652b49add8d2418a90ecae9a
Zannaivanova <Zannaivanova@tutanota.com>
2022-01-04 17:05:13 +0200

    Readline

--- phplearning.php
+++ phplearning.php
@@ -3,23 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>RADIUS</title>    
+    <title>Readline</title>    
 </head>
 <body>
-<?php //Пример #1 Использование паролей CHAP
-$radth = radius_auth_open();
-radius_add_server($radh, $server, $port, $secret, 3, 3);
-radius_create_request($radh, RADIUS_ACCESS_REQUEST);
+<?php//Пример #1 Пример использования callback-интерфейса readline
+function rl_callback($ret){
 
-$challenge = mt_rand();
+  global $c, $prompting;
 
-$ident = 1;
+  echo "Вы ввели: $ret ";
+  $c++;
+  if ($c>10){
+    $prompting = false;
+    readline_callback_handler_remove();
+  } else {
+    readline_callback_handler_install("[$c]Поговори со мной: ", 'rl_callback');
+  }
+}
+$c = 1;
+$prompting = true;
+readline_callback_handler_install("[$c] Введите что-нибудь", 'rl_callback');
 
-$cp = md5(pack('Ca*', $ident, $password.$challenge), true);
-radius_put_attr($radh, RADIUS_CHAP_PASSWORD, pack('C', $ident).$cp);
+while($prompting){
+    $w = NULL;
+    $e = NULL;
+    $n - stream_select($r = array(STDIN), $w, $e, null);
+    if ($n && in_array(STDIN, $r)){
+        readline_callback_read_char();
+    }
+}
+echo "Ввод отключеню Спасибо за внимание";
+?>
 
-radius_put_attr($radh, RADIUS_CHAP_CHELLENGE, $challenge);
- ?>
+<?php //Пример #1 Пример использования readline()
+//получим 3 команды от пользователя
+for ($i=0; $i < 3; $i++) {
+        $line = readline("Command: ");
+        readline_add_history($line);
+}
+
+//распечатаем историю ввода
+print_r(readline_list_history());
+
+//распечатаем переменные
+print_r(readline_info());
+?>
+
+
 
 <!-- https://www.php.net/manual/ru/radius.constants.attributes.php -->
 </body> 
